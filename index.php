d278ae0f1152bd948f56687dbba15d55d444da0a
miteigh <97504212+miteigh@users.noreply.github.com>
2025-01-19 19:29:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,8 +18,7 @@ session_start();
   <!-- <link href="https://miteigh.github.io/store/css/sb-admin-2.min.css" rel="stylesheet"> -->
   <!-- <link href="https://miteigh.github.io/store/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet"> -->
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
-  <link href="assets/css/bootstrap5.3.3.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.datatables.net/2.2.1/css/dataTables.dataTables.css" />
 
   <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
